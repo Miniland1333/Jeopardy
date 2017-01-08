c22089e835655a9fcec7d92af5552edcb224848d
Henry Agnew <henryd.agnew@gmail.com>
2017-01-07 20:27:01 -0800

    Spelling Fix for Add to Home Screen

--- Application/public/addtohomescreen.js
+++ Application/public/addtohomescreen.js
@@ -59,7 +59,7 @@ ath.intl = {
 
 	en_us: {
 		ios: 'To add this web app to the home screen: tap %icon and then <strong>Add to Home Screen</strong>.',
-		android: 'To add this web app to the home screen open the browser option menu and tap on <strong>Add to homescreen</strong>. <small>The menu can be accessed by pressing the menu hardware button if your device has one, or by tapping the top right menu icon %icon.</small>'
+		android: 'To add this web app to the home screen open the browser option menu and tap on <strong>Add to Home Screen</strong>. <small>The menu can be accessed by pressing the menu hardware button if your device has one, or by tapping the top right menu icon %icon.</small>'
 	},
 
 	es_es: {
