da5a6cd76436585f4e5626bd7f7b7d5f7716d58e
Henry Agnew <henryd.agnew@gmail.com>
2017-07-06 19:26:55 -0700

    Finished Dynamic Routing
    Reduces load times

--- Application/client/main.js
+++ Application/client/main.js
@@ -1,37 +1,72 @@
 import React from "react";
 import {Meteor} from "meteor/meteor";
 import {render} from "react-dom";
-import {browserHistory, Route, Router} from "react-router";
+import {Route, Router, StaticRouter, Switch} from "react-router";
+import createBrowserHistory from 'history/createBrowserHistory'
 
 import FastClick from "./fastclick";
 
-import App from "../imports/simple-todos/ui/App.jsx";
-import PageNotFound from "../imports/ui/PageNotFound";
-import Viewer from "../imports/ui/Viewer/Viewer";
-import Editor from "../imports/ui/Editor/Editor";
-import Teacher from "../imports/ui/Teacher/Teacher";
-import Student from "../imports/ui/Student/Student";
-
+const customHistory = createBrowserHistory();
 
 const renderRoutes = () => (
-	<Router history={browserHistory}>
-		<Route path="/" component={Student}/>
-		
-		<Route path="/Viewer" component={Viewer}/>
-		<Route path="/Editor" component={Editor}/>
-		<Route path="/Teacher" component={Teacher}/>
-		<Route path="/Student" component={Student}/>
-		
-		<Route path="/viewer" component={Viewer}/>
-		<Route path="/editor" component={Editor}/>
-		<Route path="/teacher" component={Teacher}/>
-		<Route path="/student" component={Student}/>
-		
-		<Route path="/App" component={App}/>
-		<Route path="/app" component={App}/>
-		<Route path="/*" component={PageNotFound}/>
+	<Router history={customHistory}>
+		<Switch>
+			<Route exact path="/" component={Student}/>
+			
+			<Route path="/Viewer" component={Viewer}/>
+			<Route path="/Editor" component={Editor}/>
+			<Route path="/Teacher" component={Teacher}/>
+			<Route path="/Student" component={Student}/>
+			
+			<Route path="/viewer" component={Viewer}/>
+			<Route path="/editor" component={Editor}/>
+			<Route path="/teacher" component={Teacher}/>
+			<Route path="/student" component={Student}/>
+			
+			<Route component={PageNotFound}/>
+		</Switch>
 	</Router>
 );
+
+const Viewer = asyncComponent(() =>
+	import("../imports/ui/Viewer/Viewer").then(module => module.default)
+);
+const Editor = asyncComponent(() =>
+	import("../imports/ui/Editor/Editor").then(module => module.default)
+);
+const Teacher = asyncComponent(() =>
+	import("../imports/ui/Teacher/Teacher").then(module => module.default)
+);
+const Student = asyncComponent(() =>
+	import("../imports/ui/Student/Student").then(module => module.default)
+);
+const PageNotFound = asyncComponent(() =>
+	import("../imports/ui/PageNotFound").then(module => module.default)
+);
+
+function asyncComponent(getComponent) {
+	return class AsyncComponent extends React.Component {
+		static Component = null;
+		state = {Component: AsyncComponent.Component};
+		
+		componentWillMount() {
+			if (!this.state.Component) {
+				getComponent().then(Component => {
+					AsyncComponent.Component = Component;
+					this.setState({Component})
+				})
+			}
+		}
+		
+		render() {
+			const {Component} = this.state;
+			if (Component) {
+				return <Component {...this.props} />
+			}
+			return null
+		}
+	}
+}
 
 Meteor.startup(() => {
 	render(renderRoutes(), document.getElementById('render-target'));
